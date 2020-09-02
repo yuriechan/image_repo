b4d1a10effcf4e099d380d9fdb307ae837e7c699
Yurie <cutecupcakeandcute@gmail.com>
2020-09-03 02:29:59 +0900

    add root endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-const greeting: string = 'Hello'
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const numbers: number[] = [1, 2, 3]
+const app: Application = express()
 
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello')
+})
+
+app.listen(5000, () => console.log('Server running'))
